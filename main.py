3990271d3f10630fc2645490973b516179b38479
JOYHEX <134592321+JOYHEX@users.noreply.github.com>
2023-07-12 11:05:26 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,92 @@
+import streamlit as st
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# custom css for the final result
+with open("style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
+
+def plot_score_analysis(df_cal):
+    # Grouped bar chart
+    fig, ax = plt.subplots()
+    bar_width = 0.35
+    index = df_cal.index
+
+    # Plotting bars
+    rects1 = ax.bar(index, df_cal['total_score'], bar_width, label='Score')
+    rects2 = ax.bar(index + bar_width, df_cal['ideal_total_score'], bar_width, label='Target Score')
+
+    # X-axis labels
+    ax.set_xticks(index + bar_width / 2)
+    ax.set_xticklabels(df_cal['cat_name'],rotation=90,fontsize=6)
+
+    # X-axis labels
+    ax.set_yticklabels(df_cal['ideal_total_score'],fontsize=6)
+    # Legend
+    ax.legend()
+
+    # Labels and title
+    ax.set_xlabel('Category')
+    ax.set_ylabel('Scores vs Target')
+    ax.set_title('Score Analysis')
+
+    # Display chart in Streamlit
+    st.pyplot(fig)
+
+
+if __name__=="__main__":
+
+    sel_option=[]
+
+    # read the file for the questions and the hints
+    st.header("Master Data Governance Maturity Evaluator")
+
+    q_file=open("questions.json")
+    input_data=json.load(q_file)
+
+    # create the drop down menu by category
+    for category in input_data:
+        with st.expander(category["category"]):
+            for question in category["QList"]:
+                id = question["name"]
+                ans=question["hints"]
+                sel_option.append(st.selectbox(
+                    question["question"],
+                    (ans),
+                    format_func=lambda x: x["q"] ,
+                    key=f"{id}",
+                    )
+                )
+        
+    df=pd.DataFrame(sel_option)
+
+
+    df_cat_r=df.groupby(["cat_ID"],as_index=False)["w"].sum()
+
+
+    # change the catedory ID with the names from the input data
+    for idx in  df_cat_r.index:
+        df_cat_r.loc[idx,["cat_name"]] = input_data[idx]["category"] 
+
+    # get the wt of each of the category
+    df_wt=df[["cat_ID","cat_wt"]].drop_duplicates()
+
+    # calculate the wt of each of the response
+    df_score= df.groupby(["cat_ID"],as_index=False)["w"].sum()
+
+    df_cal = df_wt.merge(df_score)
+    df_cal=df_cal.merge(df_cat_r)
+
+
+    # calculate the score of the question response
+    df_cal["total_score"] = df_cal["cat_wt"] * df_cal["w"] /100
+    df_cal["ideal_total_score"] = df_cal["cat_wt"] * 20 /100
+
+    # plot
+    plot_score_analysis(df_cal)
+
+    # final score display
+    final_score = df_cal[["total_score"]].sum()
+    result = final_score[0]
+    st.metric("Your current score is",round(result,2),delta=round(20-result,2))
